54f5e84bf0df872862c47745406c0aebc2e27201
ahmedgalalmohamed2016 <ahmedgalalmohamed2016@gmail.com>
2021-03-20 23:49:31 +0200

    Update main.js

--- middleware/main.js
+++ middleware/main.js
@@ -1,7 +1,7 @@
 exports.pagination = async function(req, res, next) {
     if (req.query.page) {
         req.pageNumber = req.query.page;
-        req.resultLimit = 2;
+        req.resultLimit = 25;
         return next()
     } else
         return res.status(401).send("You must enter page number");
